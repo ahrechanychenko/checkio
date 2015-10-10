bf5ceb0279443042878f749a9d9bede0e4363e13
levor23 <levor23_git@mail.ru>
2015-10-10 14:29:18 +0300

    Solution for even-last mission

---
+++ even-last.py
@@ -0,0 +1,17 @@
+__author__ = 'levor23'
+
+
+def checkio(array):
+    summary = 0
+    for x in range(0, len(array), 2):
+        summary += array[x]
+    return summary * array[-1] if len(array) > 0 else 0
+
+
+# These "asserts" using only for self-checking
+# and not necessary for auto-testing
+if __name__ == '__main__':
+    assert checkio([0, 1, 2, 3, 4, 5]) == 30, "(0+2+4)*5=30"
+    assert checkio([1, 3, 5]) == 30, "(1+5)*5=30"
+    assert checkio([6]) == 36, "(6)*6=36"
+    assert checkio([]) == 0, "An empty array = 0"
